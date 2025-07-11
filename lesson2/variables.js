3a12674fa1404cdc4535e9a3b3844094a394ea0d
Maryna Lynchak <maryna.lynchak@mgid.com>
2025-07-11 10:30:48 +0300

    first commit

---
+++ lesson2/variables.js
@@ -0,0 +1,14 @@
+// var name = 'Holly';
+// var age;
+// age = 20;
+
+// let name = 'John';
+// name = 'NewName';
+// let age;
+// age = 20;
+
+// console.log(name);
+
+const name = 'Marina';
+// name = 'newName';
+console.log(name);
