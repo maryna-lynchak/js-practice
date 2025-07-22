eb4bac34eebdc3e35d004e66bb92aa638c68a074
Maryna Lynchak <maryna.lynchak@mgid.com>
2025-07-22 15:41:41 +0300

    Add home work

---
+++ lesson5-HW/task3.js
@@ -0,0 +1,16 @@
+
+const number = 7;
+
+for (let i = 1; i <= 10; i++) {
+    let result = number * i;
+    console.log(`${number}x${i} = ${number * i}`);
+}
+
+////////////////////////////////////////////////////////
+
+let q = 1;
+
+while (q <= 10) {
+    console.log(`${number}x${q} = ${number * q}`);
+    q++;
+}
