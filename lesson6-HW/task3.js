7fa40060a845d14d362f405bf5a8c3ddad572eac
Maryna Lynchak <maryna.lynchak@mgid.com>
2025-07-22 19:41:33 +0300

    Update WH

--- lesson6-HW/task3.js
+++ lesson6-HW/task3.js
@@ -1,9 +1,9 @@
 function checkOrder (available, ordered) {
     if (ordered === 0) {
-    return "Your order is empty";;
+    return "Your order is empty";
  } else if (ordered > available ) {
      return "Your order is too large, we don’t have enough goods.";
- } else if (ordered < available) {
+ } else {
     return "Your order is accepted";
  }
 }
