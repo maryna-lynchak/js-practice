6323e055457d30b8f01abf2f5c23577271e1d0f0
Maryna Lynchak <maryna.lynchak@mgid.com>
2025-08-18 19:52:27 +0300

    Add HW8

---
+++ lesson8-HW/task2.js
@@ -0,0 +1,4 @@
+const num = [1, 2, 3, 4, 5];
+const mappedNum = num.map((num,index) => num * index);
+
+console.log (mappedNum);
---
+++ lesson8-HW/task5.js
@@ -0,0 +1,5 @@
+const firstArray = [10, 20];
+const secondArray = [30, 40];
+const concatenatedArray = firstArray.concat(secondArray);
+
+console.log(concatenatedArray); 
